e6a9b9a2dabea87f07bc80c15810e3e628188a1f
nowakjedrek <61750135+nowakjedrek@users.noreply.github.com>
2021-10-25 14:59:50 +0200

    Add files via upload

---
+++ lab3/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace lab3
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string tekst;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Zapis_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Odczyt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Dodaj_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 subwindow = new Window1();
+            subwindow.Show();
+
+        }
+    }
+}
---
+++ lab3/Window1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace lab3
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public string tekst;
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void TextBox_ID(object sender, RoutedEventArgs e)
+        {
+            ID_TextBox.Text = tekst;
+        }
+    }
+}
